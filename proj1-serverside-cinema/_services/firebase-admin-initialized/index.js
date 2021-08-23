521c869c84309f43a91a49b0f5f2afbc6cfc2c67
OilWanpen <wanpenp62@nu.ac.th>
2021-08-23 21:27:26 +0700

    [PJ1] index

--- proj1-serverside-cinema/_services/firebase-admin-initialized/index.js
+++ proj1-serverside-cinema/_services/firebase-admin-initialized/index.js
@@ -1,6 +1,5 @@
 const admin = require('firebase-admin')
 const serviceAccount = require('./service-account.json')
-require('lodash/keyBy')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
