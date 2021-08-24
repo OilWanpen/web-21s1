ccdd059de109227955b61acd5c3031789406df94
OilWanpen <wanpenp62@nu.ac.th>
2021-08-24 10:51:13 +0700

    [PJ1] Booking & Tickets

--- proj1-serverside-cinema/app.js
+++ proj1-serverside-cinema/app.js
@@ -5,6 +5,8 @@ const { json, urlencoded } = require('body-parser')
 const { index } = require('./features/index-controller')
 const { cinemaList, cinemaDetails } = require('./features/cinema-controller')
 const { filmList, filmDetails } = require('./features/film-controller')
+const { bookingForm, bookingSubmit } = require('./features/booking-controller')
+const { myTickets, myTicketsAPI } = require('./features/ticket-controller')
 
 const app = express()
 
@@ -28,5 +30,9 @@ app.get('/cinemas', cinemaList)
 app.get('/cinemas/:slug', cinemaDetails)
 app.get('/films', filmList)
 app.get('/films/:slug', filmDetails)
+app.get('/book', bookingForm)
+app.post('/book', bookingSubmit)
+app.get('/tickets', myTickets)
+app.get('/api/v1/tickets', myTicketsAPI)
 
 module.exports = { app }
