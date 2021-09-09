447708a044c8bb633724c6ec1e9a7a4f0b05edf3
OilWanpen <wanpenp62@nu.ac.th>
2021-09-09 13:04:43 +0700

    [U9] Add Buefy & MDI Icons

--- u9-css/src/main.ts
+++ u9-css/src/main.ts
@@ -1,12 +1,12 @@
 import Vue from 'vue'
+import Burfy from 'buefy'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@mdi/font/css/materialdesignicons.min.css'
+import './main.scss' 
 
 Vue.config.productionTip = false
+Vue.use(Burfy)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+new Vue({ router, store, render: h => h(App) }).$mount('#app')
