816a585f95a8ce564177ea9877f0bbc5cea1a29c
OilWanpen <wanpenp62@nu.ac.th>
2021-09-12 10:49:58 +0700

    [U9L] Book Details

--- u9-css-2/src/App.vue
+++ u9-css-2/src/App.vue
@@ -1,9 +1,72 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar class="container is-fullhd" type="is-primary">
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }" class="is-size-4">
+          <b-icon
+          icon="book-open-page-variant mr-4"
+          type="is-white">
+          </b-icon>
+          <p>BEC Books</p>
+        </b-navbar-item>
+      </template>
+
+        <template #end>
+          <b-navbar-item tag="router-link" :to="{ path: '/search' }">
+                Search
+            </b-navbar-item>
+
+          <b-navbar-item tag="router-link" :to="{ path: '/promo' }">
+                Promo
+            </b-navbar-item>
+
+          <b-navbar-dropdown right boxed collapsible hoverable>
+            <template #label>
+            <b-icon
+                icon="cart">
+            </b-icon>
+            <p>2</p>
+            </template>
+            <b-navbar-item>
+                <table>
+                  <tr v-for="book in cart" :key="book.isbn13">
+                      <img :src="`/images/books/${book.isbn13}.jpg`" :alt="book.title" />
+                      <p>{{ book.title }}<br />
+                        {{ book.price.toLocaleString() }} THB
+                      </p>
+                  </tr>
+                  </table>
+              </b-navbar-item>
+                <b-button class="is-primary" expanded>Checkout</b-button>
+          </b-navbar-dropdown>
+
+        </template>
+    </b-navbar>
+
     <router-view/>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapGetters } from 'vuex'
+import Component from 'vue-class-component'
+
+@Component({
+  computed: {
+    ...mapGetters(['cart'])
+  }
+})
+export default class BookCart extends Vue { }
+</script>
+
+<style scoped>
+.navbar-item img {
+  max-height: 3rem;
+  vertical-align: top;
+}
+
+.navbar-item p {
+  display: inline-block;
+}
+</style>
